1a49d2beec121fa9d9b71d86574efa65b3d2af33
Joshuah Ciafardone <joshuah.ciafardone@gmail.com>
2022-12-14 18:35:40 -0800

    Finished report code

--- produce_summary.py
+++ produce_summary.py
@@ -1,12 +1,13 @@
-coprint("Day 1")
+print("Day 1")
+reports = ["um-deliveries-day-1.txt","um-deliveries-day-2.txt","um-deliveries-day-3.txt"]
 the_file = open("um-deliveries-day-1.txt")
 for line in the_file:
     line = line.rstrip()
     words = line.split('|')
 
     melon = words[0]
-    count = words[0]
-    amount = words[0]
+    count = words[1]
+    amount = words[2]
 
     print(f"Delivered {count} {melon}s for total of ${amount}")
 the_file.close()
@@ -19,8 +20,8 @@ for line in the_file:
     words = line.split('|')
 
     melon = words[0]
-    count = words[0]
-    amount = words[0]
+    count = words[1]
+    amount = words[2]
 
     print(f"Delivered {count} {melon}s for total of ${amount}")
 the_file.close()
@@ -33,8 +34,8 @@ for line in the_file:
     words = line.split('|')
 
     melon = words[0]
-    count = words[0]
-    amount = words[0]
+    count = words[1]
+    amount = words[2]
 
     print(f"Delivered {count} {melon}s for total of ${amount}")
 the_file.close()
